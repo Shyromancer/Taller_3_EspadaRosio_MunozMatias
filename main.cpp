431b61e90a310bfc367e2af3351fda8bab83f05f
Shyromancer <135428893+Shyromancer@users.noreply.github.com>
2024-12-20 00:28:22 -0300

    Inicio del taller 3

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by macar on 20-12-2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by macar on 20-12-2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
